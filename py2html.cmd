bd19caebfdac83dd2a22cb2e1497a8d0164354f5
Christoph Gohlke <cgohlke@cgohlke.com>
2025-12-16 16:31:04 -0800

    Delete py2html.cmd

--- py2html.cmd
+++
@@ -1,8 +0,0 @@
-:: Convert simulate_diffusion.py to simulate_diffusion.ipynb and simulate_diffusion.html
-set PYDIR=X:\Python311
-del simulate_diffusion.html
-%PYDIR%\python.exe -m ipynb_py_convert simulate_diffusion.py simulate_diffusion.ipynb
-%PYDIR%\Scripts\jupyter-nbconvert.exe --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=600 simulate_diffusion.ipynb
-:: open HTML
-simulate_diffusion.html
-pause
